4732bc00a72aadc28c3a6cacf40a192ffaa8c635
SANDROGOMES-DEV <alex.sandro.gomes123@hotmail.com>
2026-01-15 10:51:22 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+const database = [
+    // SÃO PAULO - ZONA CENTRAL
+    { cnpj: "44649812033818", nome: "NOTRELABS ANGELICA", endereco: "AV. ANGELICA, 1045", bairro: "HIGIENOPOLIS", cidade: "SAO PAULO", estado: "SP", zona: "CENTRAL", tipo: "propria", tel: "1140901740" },
+    { cnpj: "44649812021216", nome: "QUALIVIDA ANGELICA", endereco: "AV. ANGELICA, 381", bairro: "SANTA CECILIA", cidade: "SAO PAULO", estado: "SP", zona: "CENTRAL", tipo: "propria", tel: "1140901740" },
+    { cnpj: "44649812028148", nome: "QUALIVIDA HIGIENOPOLIS", endereco: "R. SAO VICENTE DE PAULO, 463", bairro: "SANTA CECILIA", cidade: "SAO PAULO", estado: "SP", zona: "CENTRAL", tipo: "propria", tel: "1140901740" },
+    { cnpj: "44649812002696", nome: "CENTRO CLINICO POMPEIA", endereco: "AV. POMPEIA, 1165", bairro: "VILA POMPEIA", cidade: "SAO PAULO", estado: "SP", zona: "CENTRAL", tipo: "propria", tel: "1140901740" },
+    { cnpj: "45876208000107", nome: "CT DE HEMATOLOGIA DE SAO PAULO", endereco: "AV. BRIGADEIRO LUIS ANTONIO, 2533", bairro: "JD PAULISTA", cidade: "SAO PAULO", estado: "SP", zona: "CENTRAL", tipo: "credenciada", tel: "11961210622" },
+    
+    // SÃO PAULO - ZONA LESTE
+    { cnpj: "44649812022964", nome: "HOSPITAL ANALIA FRANCO", endereco: "AV. MONTEMAGNO, 241", bairro: "VILA FORMOSA", cidade: "SAO PAULO", estado: "SP", zona: "LESTE", tipo: "propria", tel: "1131552000" },
+    { cnpj: "44649812028490", nome: "HOSPITAL SALVALUS", endereco: "R. BRESSER, 1954", bairro: "MOOCA", cidade: "SAO PAULO", estado: "SP", zona: "LESTE", tipo: "propria", tel: "1140901740" },
+    { cnpj: "60742616000160", nome: "CASA DE SAUDE SANTA MARCELINA", endereco: "R. SANTA MARCELINA, 177", bairro: "ITAQUERA", cidade: "SAO PAULO", estado: "SP", zona: "LESTE", tipo: "credenciada", tel: "1120706000" },
+
+    // REGIAO ABC
+    { cnpj: "44649812005016", nome: "HOSPITAL SAO BERNARDO", endereco: "AV. LUCAS NOGUEIRA GARCEZ, 400", bairro: "CENTRO", cidade: "S. BERNARDO", estado: "SP", zona: "ABC", tipo: "propria", tel: "1140901740" },
+    { cnpj: "44649812033222", nome: "NOTRELABS MAUA", endereco: "R. GENERAL OSORIO, 228", bairro: "VILA BOCAINA", cidade: "MAUA", estado: "SP", zona: "ABC", tipo: "propria", tel: "1140901740" },
+
+    // RIO DE JANEIRO
+    { cnpj: "38058098000100", nome: "INSIGHT PSICOLOGIA CLIN MULTID", endereco: "R. PROFESSORA VENINA CORREA TORRES, 23", bairro: "CENTRO", cidade: "NOVA IGUACU", estado: "RJ", zona: "RJ", tipo: "credenciada", tel: "2141231209" },
+    { cnpj: "44649812037058", nome: "UNIDADE AVANCADA CIDADE NOVA", endereco: "R. PINTO DE AZEVEDO, 105", bairro: "CIDADE NOVA", cidade: "RIO DE JANEIRO", estado: "RJ", zona: "RJ", tipo: "propria", tel: "2140901740" },
+    { cnpj: "44649812014783", nome: "CENTRO CLINICO MADUREIRA", endereco: "R. DAGMAR DA FONSECA, 192", bairro: "MADUREIRA", cidade: "RIO DE JANEIRO", estado: "RJ", zona: "RJ", tipo: "propria", tel: "2140901740" },
+    { cnpj: "76882612005186", nome: "HOSPITAL INFANTIL JACAREPAGUA", endereco: "EST. DOS TRES RIOS, 610", bairro: "FREGUESIA", cidade: "RIO DE JANEIRO", estado: "RJ", zona: "RJ", tipo: "propria", tel: "2140901740" },
+    { cnpj: "30079461000162", nome: "CASA DE SAUDE SANTA MARTHA", endereco: "R. DOUTOR MARIO VIANA, 653", bairro: "SANTA ROSA", cidade: "NITEROI", estado: "RJ", zona: "RJ", tipo: "credenciada", tel: "2140901740" }
+];
